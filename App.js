37969fcbaa77237dcb29846f75f70675799cc059
kzya32 <kzya32@gmail.com>
2018-06-16 14:59:54 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import Home from './src/screens/Home.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/screens/Home.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    videos: [
+      { snippet: { title: 'video 1' } },
+      { snippet: { title: 'video 2' } },
+      { snippet: { title: 'video 3' } },
+    ],
+  }
+
+  componentDidMount() {
+    const channelId = 'UCa22yvSVK7nu-O7PrzTuafA';
+    const key = 'AIzaSyDQVzBPCDbkf7sMECj8viRCZ7DnGVrJeT0';
+    const maxResults = 7;
+
+    const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${key}&maxResults=${maxResults}&channelId=${channelId}&part=snippet`
+    return fetch(endpoint)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          videos: responseJson.items,
+        }, function(){
+
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItem({ item }) {
+    return (
+      <Text>
+        { item.snippet.title }
+      </Text>
+    );
+  }
+
+  render() {
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.videoList}>
+          <FlatList
+            data={this.state.videos}
+            renderItem={this.renderItem.bind(this)}
+            keyExtractor={this.keyExtractor}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Home;
